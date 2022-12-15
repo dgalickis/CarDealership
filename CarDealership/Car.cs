7c8fdf7141da8da77fb5f9104f5fb6acb8bcc402
Daniilas.Galickis <Daniilas.Galickis@LAPTOP-216>
2022-12-15 15:16:00 +0000

    Add project files.

---
+++ CarDealership/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership
+{
+    internal class Car
+    {
+        //example without constructor
+
+        public string Brand  // property
+        { get; set; }
+
+        public int Vin  // property to describe vin no.
+        { get; set; }
+
+       // public int MyProperty { get; private set; } use prop or propg tab tab to load property or encapsulated property so it cant be set from other classes
+       //controlling all the business logic.
+
+
+
+        public void RegisterWithTollbooth()
+        {
+
+        }
+
+        public void DeRegisterWithTollbooth()
+        {
+
+        }
+
+        public void SetupPayment(float amount)
+        {
+
+        }
+
+
+    }
+}
+
+
+
+//example of car with a constructor. use ctor as a shortcut to add a code snippet.
+
+
+//public string model;
+//public string color;
+//public int year;
+
+//// Create a class constructor with multiple parameters
+//public Car(string modelName, string modelColor, int modelYear)
+//{
+//    model = modelName;
+//    color = modelColor;
+//    year = modelYear;
+//}
+
+//static void Main(string[] args)
+//{
+//    Car Ford = new Car("Mustang", "Red", 1969);
+//    Console.WriteLine(Ford.color + " " + Ford.year + " " + Ford.model);
+//}
+//}
---
+++ CarDealership/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CarDealership;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Car ferrari = new Car
+        {
+            Brand = "Ferrari",
+            Vin = 39585
+        };
+        Console.WriteLine($"Here we have instantiated a car object and described two of its properties {ferrari.Brand} and {ferrari.Vin} ");
+    }
+}
